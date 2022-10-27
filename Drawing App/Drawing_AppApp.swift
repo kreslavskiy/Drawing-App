c0c28a0031d21b56872a2bb8dad2a11883d1d27f
Михайло Креславський <kreslavskiy.m@icloud.com>
2022-10-27 12:41:49 +0300

    Initial Commit

---
+++ Drawing App/Drawing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_AppApp.swift
+//  Drawing App
+//
+//  Created by Михаил Креславский on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
